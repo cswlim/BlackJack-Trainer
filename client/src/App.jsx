abff6696c6f2bd2cedbf69de564c06d00660b6df
cswlim <162635025+cswlim@users.noreply.github.com>
2025-07-29 13:25:53 -0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
-
+//this is the most stable version so far with everything.
 // --- HELPER FUNCTIONS & DATA ---
 
 const getBasicStrategy = (playerHand, dealerUpCard) => {
